38c885ed8663fbf781741c05feedf37d66d9df95
njall skarphedinsson <njallis@gmail.com>
2021-05-28 14:09:50 +0000

    fixed prizes

--- src/components/organisms/Game/EndOfRound/index.tsx
+++ src/components/organisms/Game/EndOfRound/index.tsx
@@ -9,27 +9,34 @@ import styles from "./styles";
 
 const EndOfRound = () => {
 	const auth = useSelector((state: StoreState) => state.auth);
+	const prizeCategories = useSelector(
+		(state: StoreState) => state.prize.prizeCategories
+	);
 	const dispatch = useDispatch();
 
 	return (
 		<View>
 			<Atoms.Text.Heading>Vel gert!</Atoms.Text.Heading>
 			<Atoms.Text.Para style={styles.para}>
-				Þú ert komin/n í Lvl {auth.level + 1} og ert númer {auth.scoreCard.hiscoreRank}{" "}
-				á stigatöflunni. Því meira sem þú spilar, því fleiri stigum safnarðu og því
-				fleiri og flottari vinninga áttu möguleika á að vinna.
+				Þú ert komin/n í Lvl {auth.level + 1} og ert númer{" "}
+				{auth.scoreCard.hiscoreRank} á stigatöflunni. Því meira sem
+				þú spilar, því fleiri stigum safnarðu og því fleiri og
+				flottari vinninga áttu möguleika á að vinna.
 			</Atoms.Text.Para>
 			<Atoms.Text.Para style={styles.para}>
-				Allar spurningarnar og svörin sem þú býrð til nýtast til þess að kenna
-				gervigreind að finna svör við spurningum á íslensku.
+				Allar spurningarnar og svörin sem þú býrð til nýtast til
+				þess að kenna gervigreind að finna svör við spurningum á
+				íslensku.
 			</Atoms.Text.Para>
-			{Services.Prizes.PriceCategories.map((item) => (
+			{prizeCategories.map((item) => (
 				<Atoms.Cards.PrizeCategory {...item} />
 			))}
 			<Atoms.Buttons.Base
 				type="highlight"
 				label="Áfram"
-				onPress={() => dispatch(Actions.Game.fetchCurrentGameRound())}
+				onPress={() =>
+					dispatch(Actions.Game.fetchCurrentGameRound())
+				}
 			/>
 		</View>
 	);
